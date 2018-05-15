21fdb943ab99c4db3edbb7a6c05bf3be8b4902aa
Houssein Abil <milameet@gmail.com>
2018-05-15 04:27:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var name = "Jane"
 var height = "70"
 
-if (name === "Susan") {
+if (name === "Houssein") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
+}
+
+if (height = 78) {
+  console.log("The height is correct")
+} else {
+  console.log("Expected height to be 78")
 }
 
 
