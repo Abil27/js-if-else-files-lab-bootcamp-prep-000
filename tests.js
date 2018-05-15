3d7b4dbe577903a56bb6cbd02f9602488feb9cca
Houssein Abil <milameet@gmail.com>
2018-05-15 04:24:57 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,9 +1,9 @@
 var index = require("./index.js")
 
-if (index.name === "Houssein") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Houssein, Received: " + index.name)
+    console.log("Expected: Susan, Received: " + index.name)
 }
 
 
