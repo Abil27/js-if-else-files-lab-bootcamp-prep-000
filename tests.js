ea24071a0374cb40c44c5ad9fcf97b94a845f114
Houssein Abil <milameet@gmail.com>
2018-05-15 04:22:21 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,15 +1,15 @@
 var index = require("./index.js")
 
-if (index.name === "Jane") {
+if (index.name === "Houssein") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Jane, Received: "+index.name)
+    console.log("Expected: Jane, Received: " + index.name)
 }
 
 
 
-if (index.height === 70) {
+if (index.height === 80) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 70, Received: "+ index.height)
 }
